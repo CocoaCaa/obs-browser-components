01e1bac320ee3b35d8845aadf52d138b5c9bfeab
CocoaCaa <55355513+CocoaCaa@users.noreply.github.com>
2022-01-04 16:49:25 +0000

    feat: add deer

--- sar-animal-clock/src/index.ts
+++ sar-animal-clock/src/index.ts
@@ -2,9 +2,18 @@ import { format } from 'date-fns';
 import './assets/index.css';
 import { createElementFromHtml } from './utils';
 
+const AVAILABLE_ANIMALS = ['fox', 'skullcat', 'deer'];
+
 async function main() {
   const urlSearchParams = new URLSearchParams(window.location.search);
   const animal = urlSearchParams.get('animal') ?? 'fox';
+  if (!AVAILABLE_ANIMALS.includes(animal)) {
+    const errorMessageElement = document.createElement('div');
+    errorMessageElement.textContent = `Animal "${animal}" not found. Available animals: ${AVAILABLE_ANIMALS.join(
+      ', ',
+    )}`;
+    document.body.appendChild(errorMessageElement);
+  }
   const animalResponse = await fetch(`animals/${animal}.svg`);
   document.body.appendChild(createElementFromHtml(await animalResponse.text()));
   const clockTextElement = document.querySelector<SVGTextElement>('text#clock-text');
