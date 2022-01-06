55d3a0ac68adc8c3528483603692dfedd21648cf
CocoaCaa <55355513+CocoaCaa@users.noreply.github.com>
2022-01-06 15:52:12 +0000

    feat: use Webpack and merge all code and styles to single HTML file

--- sar-animal-clock/src/utils.ts
+++
@@ -1,5 +0,0 @@
-export function createElementFromHtml<T extends Element>(html: string): T {
-  var div = document.createElement('div');
-  div.innerHTML = html.trim();
-  return div.firstChild as T;
-}
